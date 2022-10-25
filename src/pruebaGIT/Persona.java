e8c8fad84f13c213f6933e16930892265c043914
Jose Luis <jluisrr62@educastur.es>
2022-10-25 09:36:48 +0200

    segundo commit añadidos getters y setters del atributo nombre

--- src/pruebaGIT/Persona.java
+++ src/pruebaGIT/Persona.java
@@ -2,4 +2,13 @@ package pruebaGIT;
 
 public class Persona {
 	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
 }
